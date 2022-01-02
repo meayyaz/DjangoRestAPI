f2908b90057490cc595838ab28b77f2531b15e3e
Ayyaz Shaukat <ayyashaukat@gmail.com>
2022-01-03 00:45:39 +0500

    changes

--- pixel_master/settings.py
+++ pixel_master/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b0y@rf6vt-1(pt-^d&-zndh=jc8p=8^-y@j@g5u0mo@)!0*csx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
